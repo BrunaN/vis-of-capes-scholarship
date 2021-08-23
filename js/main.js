750fd564b6cabc09085299f48d5ade1805868ca8
Bruna Neves <brubsnev@gmail.com>
2021-08-23 16:19:01 -0300

    configuração da mara de círculo e timeline dos presidentes

--- js/main.js
+++ js/main.js
@@ -1,97 +1,134 @@
 const width = 1000;
 const height = 790;
 
-const map = d3.select("#map").append("svg").attr("width", width).attr("height", height);
-const lineChart = dc.compositeChart("#lineChart");
+const map = d3
+  .select('#map')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
-let g = map.append("g").attr("id", "brMap1");
+let g = map.append('g').attr('id', 'brMap1');
 
-let projection = d3.geoMercator().scale(1000).center([-54, -15]).translate([width / 2, height / 2]);
+let projection = d3
+  .geoMercator()
+  .scale(1000)
+  .center([-54, -15])
+  .translate([width / 2, height / 2]);
 
 let path = d3.geoPath().projection(projection);
 
-d3.json("./assets/files/br-states.json").then(
-    (br) => {
-        g.selectAll("path")
-            .data(topojson.feature(br, br.objects.estados).features)
-            .enter()
-            .append("path")
-            .attr("class", "state")
-            .attr("d", path);
-        
-        g.append("path")
-            .datum(topojson.mesh(br, br.objects.estados))
-            .attr("d", path)
-            .attr("class", "state_contour");
-    }
-);
+let totalByUf = new Map();
 
-d3.csv("https://raw.githubusercontent.com/JJBarata/capes_dataset/master/capes_bolsas_dataset.csv").then(
-    (data) => {
-    let parseDate = d3.timeParse("%Y");
-    data.forEach((d) => {
-        d.ano = parseDate(+d.ano);
-        d.mestrado = +d.mestrado;
-        d.doutorado_pleno = +d.doutorado_pleno;
-        d.pos_doutorado = +d.pos_doutorado;
+let circleScale = d3.scaleLinear().domain([1, 23511]).range([2, 40]);
+
+Promise.all([
+  d3.json('./assets/files/br-states.json'),
+  d3.csv('./assets/files/count-by-uf-ano.csv', function (d) {
+    if (d.ano === '2020') {
+      totalByUf.set(d.uf, +d.total_linha);
+    }
+  }),
+]).then(ready);
+
+function ready([br]) {
+  g.selectAll('path')
+    .data(topojson.feature(br, br.objects.estados).features)
+    .enter()
+    .append('path')
+    .attr('class', 'state')
+    .attr('id', (d) => d.id)
+    .attr('d', path)
+    .on('click', (e, d) => {
+      g.selectAll('circle')
+        .filter(function () {
+          return d3.select(this).attr('id') === d.id;
+        })
+        .style('fill-opacity', '1');
     });
 
-    let facts = crossfilter(data);
+  g.append('path')
+    .datum(topojson.mesh(br, br.objects.estados))
+    .attr('d', path)
+    .attr('class', 'state_contour');
 
-    facts = crossfilter(data);
+  g.selectAll('circle')
+    .data(topojson.feature(br, br.objects.estados).features)
+    .enter()
+    .append('circle')
+    .attr('transform', (d) => 'translate(' + path.centroid(d) + ')')
+    .attr('r', (d) => circleScale(totalByUf.get(d.id)))
+    .attr('class', 'circle')
+    .attr('id', (d) => d.id)
+    .on('click', (e, d) => {
+      g.selectAll('.state')
+        .filter(function () {
+          return d3.select(this).attr('id') === d.id;
+        })
+        .style('fill', '#333');
+    });
+}
 
-    ufDimension = facts.dimension(d => d.uf);
+const lineChart = dc.compositeChart('#lineChart');
 
-    totalDimension = facts.dimension(d => d.total_linha);
+d3.csv(
+  'https://raw.githubusercontent.com/JJBarata/capes_dataset/master/capes_bolsas_dataset.csv'
+).then((data) => {
+  let parseDate = d3.timeParse('%Y');
+  data.forEach((d) => {
+    d.ano = parseDate(+d.ano);
+    d.mestrado = +d.mestrado;
+    d.doutorado_pleno = +d.doutorado_pleno;
+    d.pos_doutorado = +d.pos_doutorado;
+  });
 
-    totalByUfGroup = ufDimension.group().reduceSum(d => d.total_linha);
+  let facts = crossfilter(data);
 
-    yearDimension = facts.dimension(d => d.ano);
+  facts = crossfilter(data);
 
-    masterGroup = yearDimension.group().reduceSum((d) => d.mestrado);
+  ufDimension = facts.dimension((d) => d.uf);
 
-    doctorateGroup = yearDimension.group().reduceSum((d) => d.doutorado_pleno);
+  totalDimension = facts.dimension((d) => d.total_linha);
 
-    posDocGroup = yearDimension.group().reduceSum((d) => d.pos_doutorado);
+  totalByUfGroup = ufDimension.group().reduceSum((d) => d.total_linha);
 
-    yearScale = d3.scaleTime().domain([yearDimension.bottom(1)[0].ano, yearDimension.top(1)[0].ano]);
+  yearDimension = facts.dimension((d) => d.ano);
 
-    lineChart
-        .width(width)
-        .height(450)
-        .margins({ top: 15, right: 50, bottom: 75, left: 70 })
-        .dimension(yearDimension)
-        .x(yearScale)
-        .elasticY(true)
-        .renderHorizontalGridLines(true)
-        .brushOn(false)
-        .legend(
-        dc
-            .legend()
-            .horizontal(true)
-            .x(width - 310)
-            .y(450 - 35)
-            .itemHeight(15)
-            .itemWidth(90)
-            .gap(5)
-        )
-        .compose([
-        dc
-            .lineChart(lineChart)
-            .group(masterGroup, "Mestrado")
-            .ordinalColors(["#FA8775"])
-            .curve(d3.curveCardinal),
-        dc
-            .lineChart(lineChart)
-            .group(doctorateGroup, "Doutorado")
-            .ordinalColors(["#CD34B5"])
-            .curve(d3.curveCardinal),
-        dc
-            .lineChart(lineChart)
-            .group(posDocGroup, "Pós-doutorado")
-            .ordinalColors(["#0000FF"])
-            .curve(d3.curveCardinal)
-        ]);
+  masterGroup = yearDimension.group().reduceSum((d) => d.mestrado);
 
-    dc.renderAll();
+  doctorateGroup = yearDimension.group().reduceSum((d) => d.doutorado_pleno);
+
+  posDocGroup = yearDimension.group().reduceSum((d) => d.pos_doutorado);
+
+  yearScale = d3
+    .scaleTime()
+    .domain([yearDimension.bottom(1)[0].ano, yearDimension.top(1)[0].ano]);
+
+  lineChart
+    .width(1116)
+    .height(550)
+    .margins({ top: 30, right: 50, bottom: 20, left: 70 })
+    .dimension(yearDimension)
+    .x(yearScale)
+    .elasticY(true)
+    .renderHorizontalGridLines(true)
+    .brushOn(false)
+    .compose([
+      dc
+        .lineChart(lineChart)
+        .group(masterGroup, 'Mestrado')
+        .ordinalColors(['#FA8775'])
+        .curve(d3.curveCardinal),
+      dc
+        .lineChart(lineChart)
+        .group(doctorateGroup, 'Doutorado')
+        .ordinalColors(['#CD34B5'])
+        .curve(d3.curveCardinal),
+      dc
+        .lineChart(lineChart)
+        .group(posDocGroup, 'Pós-doutorado')
+        .ordinalColors(['#0000FF'])
+        .curve(d3.curveCardinal),
+    ]);
+
+  dc.renderAll();
 });
