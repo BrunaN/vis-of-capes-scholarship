0e9ef984e14de343324838ba71aceba3101c13c0
Bruna Neves <brubsnev@gmail.com>
2021-08-25 19:01:15 -0300

    pie charts e alterações de estilo

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ function showTooltip(id, x, y) {
 }
 
 function hideTooltip() {
-  d3.select('#tooltip').classed('hidden', true);
+  d3.select('.tooltip-map').classed('hidden', true);
 }
 
 Promise.all([
@@ -75,6 +75,11 @@ Promise.all([
   }),
 ]).then(ready);
 
+function resetMap() {
+  d3.selectAll('.state').classed('selected-state', false);
+  d3.selectAll('.circle').classed('selected-circle', false);
+}
+
 function ready([br]) {
   g.selectAll('path')
     .data(topojson.feature(br, br.objects.estados).features)
@@ -83,14 +88,29 @@ function ready([br]) {
     .attr('class', 'state')
     .attr('id', (d) => d.id)
     .attr('d', path)
-    .on('click', function (e, d) {
-      d3.select(this).style('fill', '#333');
-
+    .on('mouseover', function (e, d) {
       g.selectAll('circle')
         .filter(function () {
           return d3.select(this).attr('id') === d.id;
         })
         .style('fill-opacity', '1');
+    })
+    .on('mouseout', function (e, d) {
+      g.selectAll('circle')
+        .filter(function () {
+          return d3.select(this).attr('id') === d.id;
+        })
+        .style('fill-opacity', '0.6');
+    })
+    .on('click', function (e, d) {
+      resetMap();
+      d3.select(this).classed('selected-state', true);
+
+      g.selectAll('circle')
+        .filter(function () {
+          return d3.select(this).attr('id') === d.id;
+        })
+        .classed('selected-circle', true);
 
       showTooltip(d.id, e.x, e.y);
     });
@@ -108,23 +128,83 @@ function ready([br]) {
     .attr('r', (d) => circleScale(totalByUf.get(d.id)))
     .attr('class', 'circle')
     .attr('id', (d) => d.id)
-    .on('click', function (e, d) {
-      d3.select(this).style('fill-opacity', '1');
-
+    .on('mouseover', function (e, d) {
       g.selectAll('.state')
         .filter(function () {
           return d3.select(this).attr('id') === d.id;
         })
         .style('fill', '#333');
+    })
+    .on('mouseout', function (e, d) {
+      g.selectAll('.state')
+        .filter(function () {
+          return d3.select(this).attr('id') === d.id;
+        })
+        .style('fill', 'transparent');
+    })
+    .on('click', function (e, d) {
+      resetMap();
+      d3.select(this).classed('selected-circle', true);
+
+      g.selectAll('.state')
+        .filter(function () {
+          return d3.select(this).attr('id') === d.id;
+        })
+        .classed('selected-state', true);
 
       showTooltip(d.id, e.x, e.y);
     });
 }
 
 const lineChart = dc.compositeChart('#lineChart');
+const pieChartLevel = dc.pieChart('#pieChartLevel');
+const pieChartStatus = dc.pieChart('#pieChartStatus');
+
+function regroup(dimension, columns) {
+  let _groupAll = dimension.groupAll().reduce(
+    function (p, v) {
+      columns.forEach(function (c) {
+        p[c] += v[c];
+      });
+      return p;
+    },
+    function (p, v) {
+      columns.forEach(function (c) {
+        p[c] -= v[c];
+      });
+      return p;
+    },
+    function () {
+      let p = {};
+      columns.forEach(function (c) {
+        p[c] = 0;
+      });
+      return p;
+    }
+  );
+  return {
+    all: function () {
+      let data = [];
+      for (let [key, value] of Object.entries(_groupAll.value())) {
+        data.push({ key: key, value, value });
+      }
+      return data;
+    },
+  };
+}
+
+levelColorScale = d3
+  .scaleOrdinal()
+  .domain(['mestrado', 'doutorado_pleno', 'pos_doutorado'])
+  .range(['#3263FD', '#FEB865', '#D8584E']);
+
+statusColorScale = d3
+  .scaleOrdinal()
+  .domain(['Federal', 'Estadual', 'Privada', 'Municipal'])
+  .range(['#04A6EA', '#FA8775', '#329694', '#5D5988']);
 
 d3.csv(
-  'https://raw.githubusercontent.com/JJBarata/capes_dataset/master/capes_bolsas_dataset.csv'
+  'https://raw.githubusercontent.com/JJBarata/capes_dataset/master/dataset_bolsas_capes.csv'
 ).then((data) => {
   let parseDate = d3.timeParse('%Y');
   data.forEach((d) => {
@@ -132,34 +212,48 @@ d3.csv(
     d.mestrado = +d.mestrado;
     d.doutorado_pleno = +d.doutorado_pleno;
     d.pos_doutorado = +d.pos_doutorado;
+    d.status_juridico = d.status_juridico;
   });
 
   let facts = crossfilter(data);
 
-  facts = crossfilter(data);
-
   ufDimension = facts.dimension((d) => d.uf);
 
-  totalDimension = facts.dimension((d) => d.total_linha);
+  totalDimension = facts.dimension((d) => +d.total_linha);
 
   totalByUfGroup = ufDimension.group().reduceSum((d) => d.total_linha);
 
   yearDimension = facts.dimension((d) => d.ano);
 
-  masterGroup = yearDimension.group().reduceSum((d) => d.mestrado);
+  masterByYearGroup = yearDimension.group().reduceSum((d) => d.mestrado);
 
-  doctorateGroup = yearDimension.group().reduceSum((d) => d.doutorado_pleno);
+  doctorateByYearGroup = yearDimension
+    .group()
+    .reduceSum((d) => d.doutorado_pleno);
 
-  posDocGroup = yearDimension.group().reduceSum((d) => d.pos_doutorado);
+  posDocByYearGroup = yearDimension.group().reduceSum((d) => d.pos_doutorado);
+
+  levelsDimension = facts.dimension(function (d) {
+    return d;
+  });
+
+  levelsGroup = regroup(levelsDimension, [
+    'mestrado',
+    'doutorado_pleno',
+    'pos_doutorado',
+  ]);
+
+  statusDimension = facts.dimension((d) => d.status_juridico);
+  statusGroup = statusDimension.group().reduceCount();
 
   yearScale = d3
     .scaleTime()
     .domain([yearDimension.bottom(1)[0].ano, yearDimension.top(1)[0].ano]);
 
   lineChart
-    .width(1116)
+    .width(1250)
     .height(550)
-    .margins({ top: 30, right: 50, bottom: 20, left: 70 })
+    .margins({ top: 50, right: 50, bottom: 20, left: 70 })
     .dimension(yearDimension)
     .x(yearScale)
     .elasticY(true)
@@ -168,20 +262,68 @@ d3.csv(
     .compose([
       dc
         .lineChart(lineChart)
-        .group(masterGroup, 'Mestrado')
-        .ordinalColors(['#FA8775'])
-        .curve(d3.curveCardinal),
+        .group(masterByYearGroup, 'Mestrado')
+        .colors(levelColorScale)
+        .curve(d3.curveCardinal)
+        .renderDataPoints({ radius: 5, fillOpacity: 1 }),
       dc
         .lineChart(lineChart)
-        .group(doctorateGroup, 'Doutorado')
-        .ordinalColors(['#CD34B5'])
-        .curve(d3.curveCardinal),
+        .group(doctorateByYearGroup, 'Doutorado')
+        .colors(levelColorScale)
+        .curve(d3.curveCardinal)
+        .renderDataPoints({ radius: 5, fillOpacity: 1 }),
       dc
         .lineChart(lineChart)
-        .group(posDocGroup, 'Pós-doutorado')
-        .ordinalColors(['#0000FF'])
-        .curve(d3.curveCardinal),
+        .group(posDocByYearGroup, 'Pós-doutorado')
+        .colors(levelColorScale)
+        .curve(d3.curveCardinal)
+        .renderDataPoints({ radius: 5, fillOpacity: 1 }),
     ]);
+
+  pieChartLevel
+    .width(400)
+    .height(400)
+    .dimension(levelsDimension)
+    .group(levelsGroup)
+    .innerRadius(100)
+    .colors(levelColorScale)
+    .on('pretransition', function (chart) {
+      chart.selectAll('text.pie-slice').text(function (d) {
+        return (
+          dc.utils.printSingleValue(
+            ((d.endAngle - d.startAngle) / (2 * Math.PI)) * 100
+          ) + '%'
+        );
+      });
+    });
+
+  pieChartLevel.filterHandler(function (dim, filters) {
+    if (filters && filters.length)
+      levelsDimension.filterFunction(function (r) {
+        return filters.some(function (c) {
+          return r[c] > 0;
+        });
+      });
+    else dim.filterAll();
+    return filters;
+  });
+
+  pieChartStatus
+    .width(400)
+    .height(400)
+    .dimension(statusDimension)
+    .group(statusGroup)
+    .innerRadius(100)
+    .colors(statusColorScale)
+    .on('pretransition', function (chart) {
+      chart.selectAll('text.pie-slice').text(function (d) {
+        return (
+          dc.utils.printSingleValue(
+            ((d.endAngle - d.startAngle) / (2 * Math.PI)) * 100
+          ) + '%'
+        );
+      });
+    });
 
   dc.renderAll();
 });
